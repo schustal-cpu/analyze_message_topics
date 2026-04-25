72bb52029e03cd0a7174d8c0a7ccf0a15f640e27
Lisa Schuster <schustal92@gmail.com>
2026-04-25 13:23:49 +0200

    get_data_fragdenstaat finalisiert

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# analyze_message_topics
+# analyze\_message\_topics
+
+
+
+Uses the following two Datasets for analyzing:
+
+* formal Complaints (german):
+https://fragdenstaat.de/api/v1/request/
+Downloaded via get\_data\_fragdenstaat.py and stored in data/fragdenstaat\_messages\_raw.json/.csv
+
+&#x20;  
+
+* social Media Messages (english):
+https://business.yelp.com/data/resources/open-dataset/
+Manual Downloaded and stored in data/yelp\_messages\_raw.json
+
--- get_data_fragdenstaat.py
+++ get_data_fragdenstaat.py
@@ -14,43 +14,98 @@
 # ---
 
 import requests
+from datetime import datetime
 import pandas as pd
 from pathlib import Path
 import time
 import json
+from pprint import pprint
+
+
+def main():
+
+    local_dir = Path("data/")
+    local_dir.mkdir(parents=True, exist_ok=True)
+    
+    base_url = "https://fragdenstaat.de/api/v1/request/"
+    num_entries = 1000 #Anzahl Einträge die gesammelt werden sollen
+    sel_state = 92 #Bayern vgl. https://fragdenstaat.de/api/v1/jurisdiction/
+    
+    path_raw = local_dir / "fragdenstaat_messages_raw.json"
+    path_csv = local_dir / "fragdenstaat_messages.csv"
+
+    data = fetch_entries(base_url, num_entries, sel_state)
+    save_raw_json(data, path_raw)
+    extract_to_csv(data, path_csv)
+  
 
 
 # +
-def fetch_100_entries(base_url):
+def fetch_entries(url, num_entries, state=None):
     """
-    Fetches 100 request entries from FragDenStaat using pagination.
-    Only available objects are collected until the limit is reached.
+    Fetches up to num_entries filtered entries from FragDenStaat API.
+    Uses pagination via 'meta.next'.
     """
+
     results = []
-    page = 1
 
-    print("Starte Download von bis zu 100 Einträgen...")
+    params = {
+        "limit": 100  # maximale Seitengröße aus Performancegründen
+    }
 
-    while len(results) < 100:
-        url = f"{base_url}?page={page}&format=json"
+    # Filterung nach Bundesland, wenn state nicht übergeben wird, keine Filterung
+    if state:
+        params["jurisdiction"] = state
 
-        r = requests.get(url)
+    # Kamagnen die ignoriert werden sollen
+    # Vorabfilterung da sonst zu 90% der Daten nur anfragen über die Abiturprüfungsergebnisse diverser Jahre enthalten und keine sinvolle Themenprüfung stattfinden kann.
+    excluded_campaigns = {
+        "https://fragdenstaat.de/api/v1/campaign/15/", # Kampagne: "Verschlusssache Prüfung"
+        "https://fragdenstaat.de/api/v1/campaign/5/" # Kampagne: "Frag sie Abi!"
+    }
+
+    print(f"Starte Download von bis zu {num_entries} Einträgen...")
+
+    # 
+    while url:
+        r = requests.get(url, params=params)
         if r.status_code != 200:
             print(f"API Fehler {r.status_code} bei URL: {url}")
             break
 
-        objects = r.json().get("objects", [])
+        data = r.json()
+        objects = data.get("objects", [])
+
+        # Abbruch, wenn keine weiteren Daten in der API gefunden, bzw. auf der letzten Seite angekommen.
         if not objects:
             print("Keine weiteren Daten gefunden.")
             break
 
-        results.extend(objects)
-        page += 1
-        time.sleep(0.3)
+        # Eingelesene Daten nach Kampagnen filtern
+        for entry in objects:
+            if entry.get("campaign") not in excluded_campaigns:
+                results.append(entry)
 
-    print(f"{len(results[:100])} Einträge heruntergeladen.")
-    return results[:100]
+        # Abbruch, wenn Anzahl gewünschter Ergebnisse erreicht ist
+        if len(results) >= num_entries:
+            break
 
+        # Auf die nächte API Seite wechseln, da Anfragen auf mehrere Siten aufgeteilt sind.
+        url = data.get("meta", {}).get("next")
+        params = None  # Parameter werden nur im ersten Request benötigt
+        
+        print(f"Current Entries: {len(results)} -> going to Next page")
+
+        time.sleep(0.2)
+
+    print(f"{len(results[:num_entries])} Einträge heruntergeladen.")
+    return results[:num_entries]
+
+def format_created(ts):
+    if not ts:
+        return None
+    dt = datetime.fromisoformat(ts)
+    return dt.strftime("%d.%m.%Y %H:%M")
 
 def extract_to_csv(data, csv_path):
     """
@@ -58,7 +113,10 @@ def extract_to_csv(data, csv_path):
     """
     rows = [
         {
+            "origin": "FragdenStaat",
             "id": d.get("id"),
+            "created": format_created(d.get("created_at")),
+            "title": (d.get("title") or "").strip(),
             "description": (d.get("description") or "").strip()
         }
         for d in data
@@ -79,25 +137,9 @@ def save_raw_json(data, json_path):
     print(f"RAW JSON gespeichert unter: {json_path}")
 
 
-
-# +
-def main():
-
-    local_dir = Path("data/")
-    local_dir.mkdir(parents=True, exist_ok=True)
-    
-    base_url = "https://fragdenstaat.de/api/v1/request/"
-    
-    path_raw = local_dir / "fragdenstaat_messages_raw.json"
-    path_csv = local_dir / "fragdenstaat_messages.csv"
-
-    data = fetch_100_entries(base_url)
-    save_raw_json(data, path_raw)
-    extract_to_csv(data, path_csv)
-
+# -
 
 if __name__ == "__main__":
     main()
-# -
-
+    
 
